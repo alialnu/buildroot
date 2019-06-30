14e2de5216fafb8508eb6e0ff1e25719b47b925b
Ali Nubani <ali.alnubani94@gmail.com>
2019-06-30 20:10:27 +0300

    package/python-sphinxcontrib-jsmath: new package
    A sphinx extension which renders display math in HTML via JavaScript.
    
    This package is required by sphinx.

---
+++ package/python-sphinxcontrib-jsmath/python-sphinxcontrib-jsmath.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-sphinxcontrib-jsmath
+#
+################################################################################
+
+PYTHON_SPHINXCONTRIB_JSMATH_VERSION = 1.0.1
+PYTHON_SPHINXCONTRIB_JSMATH_SOURCE = sphinxcontrib-jsmath-$(PYTHON_SPHINXCONTRIB_JSMATH_VERSION).tar.gz
+PYTHON_SPHINXCONTRIB_JSMATH_SITE = https://files.pythonhosted.org/packages/b2/e8/9ed3830aeed71f17c026a07a5097edcf44b692850ef215b161b8ad875729
+PYTHON_SPHINXCONTRIB_JSMATH_SETUP_TYPE = setuptools
+PYTHON_SPHINXCONTRIB_JSMATH_LICENSE = BSD-2-Clause
+PYTHON_SPHINXCONTRIB_JSMATH_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
